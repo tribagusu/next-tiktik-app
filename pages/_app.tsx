838ceb5ff8e46d5eb99008fd3f4d9c265726c728
tribagus <tri.bagus21@gmail.com>
2022-11-14 11:15:35 +0700

    sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState, useEffect } from "react"
+import type { AppProps } from "next/app"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css"
+import Navbar from "../components/Navbar"
+import Sidebar from "../components/Sidebar"
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true)
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if (isSSR) return null
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default MyApp
