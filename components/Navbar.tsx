2cd9d4342614e3313bf2e5079023c7f15f4a3bf4
tribagus <tri.bagus21@gmail.com>
2022-11-15 11:13:22 +0700

    video card

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,23 @@
 import React from "react"
 import Image from "next/image"
 import Link from "next/link"
+import { GoogleLogin, googleLogout } from "@react-oauth/google"
 import { useRouter } from "next/router"
-import { GoogleLogin, GoogleLogout } from "react-google-login"
 import { AiOutlineLogout } from "react-icons/ai"
 import { BiSearch } from "react-icons/bi"
 import { IoMdAdd } from "react-icons/io"
 
-import Logo from "../utils/tiktik-logo.png"
+import Logo from "../utils/tiktok-logo.png"
+import { createOrGetUser } from "../utils"
 
 const Navbar = () => {
+  const user = false
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
         <div className="w-[100px] md:w-[130px] ">
           <Image
+            priority={true}
             className="cursor-pointer"
             src={Logo}
             alt="tiktik"
@@ -22,6 +25,19 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
     </div>
   )
 }
